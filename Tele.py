9d2d160617d75055f2ec882557058e1580928037
Ganesh <99340632+Ganesh-36-12@users.noreply.github.com>
2024-08-28 16:54:58 +0530

    Update Tele.py

--- Tele.py
+++ Tele.py
@@ -3,17 +3,19 @@ import os
 import logging
 import logging.handlers
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger_file_handler = logging.handlers.RotatingFileHandler(
-    "status.log",
-    maxBytes=1024 * 1024,
-    backupCount=1,
-    encoding="utf8",
-)
-formatter = logging.Formatter("%(asctime)s - %(message)s")
-logger_file_handler.setFormatter(formatter)
-logger.addHandler(logger_file_handler)
+def log_event(msg):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    logger_file_handler = logging.handlers.RotatingFileHandler(
+        "status.log",
+        maxBytes=1024 * 1024,
+        backupCount=1,
+        encoding="utf8",
+    )
+    formatter = logging.Formatter("%(asctime)s - %(message)s")
+    logger_file_handler.setFormatter(formatter)
+    logger.addHandler(logger_file_handler)
+    logger.info(msg)
 
 try:
     API_KEY = os.environ["API_KEY"]
