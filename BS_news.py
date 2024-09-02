1b3df2a852b8038567d34ac1d59850c723d90582
Ganesh <99340632+Ganesh-36-12@users.noreply.github.com>
2024-09-02 15:36:58 +0530

    Update BS_news.py

--- BS_news.py
+++ BS_news.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime , timedelta
 from Tele import send_msg
-import sqlite3 as sq
+from Database import *
 
 raw_time = datetime.utcnow()+timedelta(hours=5,minutes=30)
 current_time = raw_time.strftime("%H:%M")
@@ -12,56 +12,39 @@ current_hour_windows = raw_time.strftime("%#I")
 AM_PM = raw_time.strftime("%p")
 current_date = raw_time.strftime("%d %b %Y")
 
-# try:
-#     conn = sq.connect('info_collector_BS.db')
-#     cursor = conn.cursor()
-#     create_command = """
-#     CREATE TABLE IF NOT EXISTS 
-#     SCRAPED (NEWS STRING NOT NULL);
-#     """
-#     cursor.execute(create_command)
-# except Exception as e:
-#     print(e)
-    
-def insert_data_into_db(n_list):
-    try:
-        insert_command = """
-        INSERT INTO SCRAPED (NEWS) VALUES(?);
-        """
-        last = (n_list[0],)
-        cursor.execute(insert_command,last)
-        conn.commit()
-    except Exception as e:
-        print(e)
-
+create_tables()
 
 def website(site_url):
-  headers = {
+    headers = {
       'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"
       }
+    old_news = fetch_old_news()
+    r = requests.get(url=site_url,headers=headers)
+    status = r.status_code
+    if status ==200:
+        list_1 = web_crawl(r,old_news)
+        return list_1
 
-  r = requests.get(url=site_url,headers=headers)
-  status = r.status_code
-  if status ==200:
-    dict_1 = web_crawl(r)
-    return dict_1
-
-def web_crawl(r):
-  soup = BeautifulSoup(r.content, 'html.parser')
-  job_cards = soup.find_all('div', class_='listingstyle_cardlistlist__dfq57 cardlist')
-
-  title_list =[]
-
-  for jobs in job_cards:
-    title = jobs.find('a').text
-    time_cards = jobs.find_all('span',class_='listingstyle_updtText__lnZb7')
-    for time in time_cards:
-      time_stamp = time.span.text
-      date = time_stamp[13:24]
-      upload_time = time_stamp[26:35].strip()
-      if (upload_time.startswith(current_hour_unix)) and upload_time.endswith(AM_PM) and date.startswith(current_date):
-        title_list.append(title)
-  return title_list
+def web_crawl(r,l_news):
+    soup = BeautifulSoup(r.content, 'html.parser')
+    job_cards = soup.find_all('div', class_='listingstyle_cardlistlist__dfq57 cardlist')
+    title_list =[]
+    
+    for jobs in job_cards:
+        title = jobs.find('a').text
+        time_cards = jobs.find_all('span',class_='listingstyle_updtText__lnZb7')
+        for time in time_cards:
+            time_stamp = time.span.text
+            date = time_stamp[13:24]
+            upload_time = time_stamp[26:35].strip()
+            if date.startswith(current_date):
+                if(title==l_news):
+                    insert_data_into_db("BS_SCRAPED",title_list)
+                    return title_list
+                else:
+                     title_list.append(title)
+    insert_data_into_db("BS_SCRAPED",title_list)
+    return title_list
 
 def string_builder(news):
     final_string = current_hour+' News update\n'
@@ -75,9 +58,11 @@ def string_builder(news):
 
 news_list =[]
 for i in range(1,4):
-  url = f"https://www.business-standard.com/markets/news/page-{i}"
-  temp = website(url)
-  news_list.extend(temp)
+    url = f"https://www.business-standard.com/markets/news/page-{i}"
+    temp = website(url)
+    news_list.extend(temp)
+    insert_data_into_db("scraped",news_list)
+    
 
 text = string_builder(news_list)
 print(text)
