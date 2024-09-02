23d95d92452a9ab87a07eed0b3107d332df777a2
Ganesh <99340632+Ganesh-36-12@users.noreply.github.com>
2024-09-02 19:28:50 +0530

    Update forwarder.py

--- forwarder.py
+++ forwarder.py
@@ -4,12 +4,12 @@ from Tele import *
 from Database import *
 
 try:
-  API_KEY = os.environ["API_KEY"]
-  chat_id = os.environ["METRICS_GROUP_ID"]
-  from_id = os.environ["METRICS_CHANNEL_ID"]
+  FORWARDER_API_KEY = os.environ["FORWARDER_API_KEY"]
+  METRICS_GROUP_ID = os.environ["METRICS_GROUP_ID"]
+  METRICS_CHANNEL_ID = os.environ["METRICS_CHANNEL_ID"]
 except KeyError:
   logger.info("Token not available!")
 
-base_url = f'https://api.telegram.org/bot{API_KEY}'
+base_url = f'https://api.telegram.org/bot{FORWARDER_API_KEY}'
 
-forward_msg(base_url,chat_id,from_id)
+forward_msg(base_url,METRICS_GROUP_ID,METRICS_CHANNEL_ID)
