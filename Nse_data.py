84fc3ccb41b1af92d36fa5fb5a0d6292658937f7
Ganesh <99340632+Ganesh-36-12@users.noreply.github.com>
2024-10-17 11:29:51 +0530

    removed NSEPython

--- Nse_data.py
+++ Nse_data.py
@@ -1,26 +1,37 @@
-from nsepython import *
+
+import requests
+import json
 from Tele import send_msg
 
-
+headers = {
+      'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"
+      }
 def fiidii():
-    list_1=nse_fiidii("list")
-   
-    message = (f"{list_1[0]['date']}\n")
-    for i in list_1:
-        message += (f"category : {i['category']}\n")
-        message += (f"Buy value : {i['buyValue']}\n")
-        message +=(f"Sell value : {i['sellValue']}\n")
-        message +=(f"Net value : {i['netValue']}\n\n")
-    return message
+  site_url = 'https://www.nseindia.com/api/fiidiiTradeReact'
+  r = requests.get(url=site_url,headers=headers).json()
+  message = (f"{r[0]['date']}\n")
+  for i in r:
+      message += (f"category : {i['category']}\n")
+      message += (f"Buy value : {i['buyValue']}\n")
+      message +=(f"Sell value : {i['sellValue']}\n")
+      message +=(f"Net value : {i['netValue']}\n\n")
+  return message
+
+def get_vix():
+  site_url = 'https://www.nseindia.com/api/allIndices'
+  response = requests.get(url=site_url,headers=headers).json()
+  temp_list = response['data']
+  for i in temp_list:
+    if(i['index']=="INDIA VIX"):
+      return i
     
 def india_VIX():
-    vix_data = nse_get_index_quote('INDIA VIX')
-        
-    last= float(vix_data['last'])
-    diff = last - float(vix_data['open'])
-    vix_msg= (f"India VIX : {last:.2f} {diff:.2f} ({vix_data['percChange']}%)")
-    return vix_msg
-    
+  vix_data = get_vix()
+  last= float(vix_data['last'])
+  diff = last - float(vix_data['open'])
+  vix_msg= (f"India VIX : {last:.2f} {diff:.2f} ({vix_data['percentChange']}%)")
+  return vix_msg
+  
     
 msg = fiidii()
 vix = india_VIX()
