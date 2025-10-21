18fb2c1469ac23f0afb3de49f7212c38096beb99
Ganesh <99340632+Ganesh-36-12@users.noreply.github.com>
2025-10-21 09:16:16 +0530

    Update mc_extractor.py

--- mc_extractor.py
+++ mc_extractor.py
@@ -1,104 +1,101 @@
-import re
-import os
+
+import json
 import requests
+from fake_headers import Headers
 from bs4 import BeautifulSoup
-from datetime import datetime, timedelta
+from datetime import date
 from Tele import *
-from googlesearch import *
 
-def todayArticle(soup):
-  raw_time = datetime.utcnow()+timedelta(hours=5,minutes=30)
-  current_date = raw_time.strftime("%B %d, %Y")
+today = date.today()
+current_date = today.strftime("%B %d, %Y")
 
-  upload_date = soup.find('div',class_="article_schedule").span.text
-  if current_date == upload_date:
-    return True
-  return False
+url ="https://api.moneycontrol.com/mcapi/v1/premarket/article?slug=trade-setup&limit=1"
+headers =  Headers(headers=True,os='win').generate()
 
-def get_images(soup):
-  image_cont = soup.find('div',{'id':'contentdata'})
-  images = image_cont.find_all('img',{'class':"aligncenter"})
-  image_dict = {}
-  image_dict["NIFTY 50"] = images[0].get('src')
-  image_dict["Bank NIFTY"] = images[1].get('src')
+r =  requests.get(url,headers=headers)
+
+if r.status_code == 200:
+  data = r.json()
+  article = json.loads(data['data']['trade_setup'][0]['article_data'])
+  post_url = article['posturl']
+
+  news = article['body'].replace("\r","").replace(",","")
+
+  soup = BeautifulSoup(news,'html.parser')
+
+
+def get_images():
+  image_dict ={}
+  img = soup.find_all('img')
+  image_dict["NIFTY 50"] = img[0].get('src')
+  image_dict["Bank NIFTY"] = img[1].get('src')
   return image_dict
 
-def number_extractor(data_list,r_s):
-  count = 1
-  temp_string = ''
-  for i in data_list:
-    cs = i.replace(",",'')
-    if cs.isdigit():
-      temp_string += f"*{r_s}{count}*: {cs} "
-      count +=1
-  return temp_string
+def number_extractor(points_list,r_s):
+    count = 1
+    temp_string = ''
+    for point in points_list:
+        if point.isdigit():
+            temp_string += f"*{r_s}{count}*: {point} "
+            count +=1
+    return temp_string
 
-def resistance_support(soup):
-  resistance = soup.find_all('p',string=re.compile("^Resistance based on pivot points:"))
-  nifty_r_data = resistance[0].text[34:].split()
-  bank_nifty_r_data = resistance[1].text[34:].split()
+def resistance_support():
+    splitted = news.split("\n")
+    resistance_data = []
+    support_data = []
+    for lines in splitted:
+        if lines.startswith("Resistance based on pivot points:"):
+            resistance_data.append(lines.split(": ")[1])
+        if lines.startswith("Support based on pivot points:"):
+            support_data.append(lines.split(": ")[1])
+        
+    nifty_r_data = resistance_data[0].split()
+    bank_nifty_r_data = resistance_data[1].split()
 
-  support = soup.find_all('p',string=re.compile("^Support based on pivot points:"))
-  nifty_s_data = support[0].text[31:].split()
-  bank_nifty_s_data = support[1].text[31:].split()
+    nifty_s_data = support_data[0].split()
+    bank_nifty_s_data = support_data[1].split()
 
-  content_dict ={"nifty":
-                        {"resistance":{},
-                        "support":{}
-                        },
-                  "bank_nifty":
-                        {"resistance":{},
-                        "support":{}
-                        }
-                }
-  content_dict["nifty"]["resistance"] = number_extractor(nifty_r_data,"R")
-  content_dict["bank_nifty"]["resistance"] = number_extractor(bank_nifty_r_data,"R")
-  content_dict["nifty"]["support"] = number_extractor(nifty_s_data,"S")
-  content_dict["bank_nifty"]["support"] = number_extractor(bank_nifty_s_data,"S")
-  return content_dict
+    content_dict = {"nifty":{"resistance":{},"support":{}},
+                "bank_nifty":{"resistance":{},"support":{}}}
+    
+    content_dict["nifty"]["resistance"] = number_extractor(nifty_r_data,"R")
+    content_dict["bank_nifty"]["resistance"] = number_extractor(bank_nifty_r_data,"R")
+    content_dict["nifty"]["support"] = number_extractor(nifty_s_data,"S")
+    content_dict["bank_nifty"]["support"] = number_extractor(bank_nifty_s_data,"S")
+    return content_dict
 
-def f_o_ban(soup):
-  retain = soup.find('p',string=re.compile("^Stocks retained in F&O ban:"))
-  added = soup.find('p',string=re.compile("^Stocks added to F&O ban:"))
+
+def f_o_ban():
+  f_o = news.replace("&amp;","&").split("\n")
   ban_string = "*Stocks retained in F&O ban:*\n"
-  for cmp in retain.text[28:].split(","):
-    ban_string += cmp.strip() + "\n"
-  for org in added.text[24:].split(","):
-    ban_string += org.strip() + "\n"
+  for i in f_o:
+      if i.startswith("Stocks added") or i.startswith("Stocks retained"):
+          banned = i.split(": ")[1]
+          if banned != "Nil":
+              ban_string += banned
 
   return ban_string
 
-raw_time = datetime.utcnow()+timedelta(hours=5,minutes=30)
-current_date = raw_time.strftime("%B %d, %Y")
 
-query = "moneycontrol top 15 things to do latest"
-
-results = search(query=query,num=1,stop=1)
-
-link = [x for x in results]
-mc_url = link[0]
-headers = {
-      'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"
-      }
-mc_resp = requests.get(url=mc_url,headers=headers)
-print(mc_resp.status_code)
-mc_soup = BeautifulSoup(mc_resp.content, 'html.parser')
-
-def string_builder(soup):
-  content_dict = resistance_support(soup)
+def string_builder():
+  content_dict = resistance_support()
   final = f"{current_date}\nNIFTY\n"
   final += content_dict["nifty"]["resistance"] + "\n"
   final += content_dict["nifty"]["support"] + "\n\n"
   final += f"BANK NIFTY\n"
   final += content_dict["bank_nifty"]["resistance"] + "\n"
   final += content_dict["bank_nifty"]["support"] + "\n\n"
+  
   return final
 
 try:
-  print("today's link: ",mc_url)
-  final_string = string_builder(mc_soup) + f_o_ban(mc_soup)
+  print("today's link: ",post_url)
+  final_string = string_builder() + f_o_ban()
   send_styled_msg(final_string,"mc_extractor")
-  image_dict = get_images(mc_soup)
+  print(final_string)
+  image_dict = get_images()
+  print(image_dict)
   send_photo(image_dict,"mc_extractor")
 except Exception as e:
   print(e)
